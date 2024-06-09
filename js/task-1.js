8d19ba4f47bc46b4409ea5fc4ad073be66b710ea
Maryna H <maryna.hamici@yahoo.com>
2024-06-09 21:19:52 +0200

    final changes

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,6 @@
 function makeTransaction (quantity, pricePerDroid){
     const totalPrice = quantity * pricePerDroid;
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+    return `"You ordered ${quantity} droids worth ${totalPrice} credits!"`;
 }
 console.log(makeTransaction(5, 3000));
 console.log(makeTransaction(3, 1000));
